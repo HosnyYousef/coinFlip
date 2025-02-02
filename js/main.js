e5717b60956b5a6a47d867cc6057a1664057e02a
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-02-02 13:41:04 +0200

    psuedo Code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 
+
+// click button, show one of the images
+// calculation to show image 50%
+// 
+
+
+// eventListener for button
+    // if clicked, iniate calcualtion
+    // if clicked again, reset
+        //and iniate calcualtion again
+    // 
+
+const coin = document.querySelector('')
