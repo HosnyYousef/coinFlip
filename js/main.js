187ebd68778897b5ec38d14054d9cb7b4e1bd5c4
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-02-02 16:14:15 +0200

    JavaScript

--- js/main.js
+++ js/main.js
@@ -8,18 +8,31 @@
         //and iniate calcualtion again
     // 
 
+//on click: refresh page
+    const refreshBtn = document.getElementById("btnRefresh");
+
+    function handleClick() {
+      window.location.reload();
+    }
+    
+    refreshBtn.addEventListener("click", handleClick);
+    
+//on click: flip coin
     document.querySelector('#flipButton').addEventListener('click', flip)
 
-    function flip (){
+    function flip (event){
         let heads = document.querySelector('#heads')
         let tails = document.querySelector('#tails')
+        let result = document.getElementsByClassName("coin");
         var x = Math.floor(Math.random() * 2);
         console.log(x);
         if (x == 0) {
         document.querySelector('#heads').classList.toggle = 'hidden'
+        result.innerHTML= "<img alt=\"the frontside of a coin\" src=\"img/heads.webp\" />";
         tails.style.display = 'none'
-    } else {
+    } else if (x == 1){
         document.querySelector('#tails').classList.toggle = 'hidden'
+        result.innerHTML= "<img alt=\"the backside of a coin\" src=\"img/tails.webp\" />";
         heads.style.display = 'none'
       }
 }
