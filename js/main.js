46fbaa1263f352d188f668943da8658febec5434
Yousef Hosny <Yousefhosny.hm@gmail.com>
2025-02-02 17:11:12 +0200

    JavaScript

--- js/main.js
+++ js/main.js
@@ -3,38 +3,40 @@
 // calculation to show image 50%
 
 // eventListener for button
-    // if clicked, iniate calcualtion
-    // if clicked again, reset
-        //and iniate calcualtion again
-    // 
+// if clicked, iniate calcualtion
+// if clicked again, reset
+//and iniate calcualtion again
+// 
 
 //on click: refresh page
-    const refreshBtn = document.getElementById("btnRefresh");
+// const refreshBtn = document.getElementById("btnRefresh");
 
-    function handleClick() {
-      window.location.reload();
-    }
-    
-    refreshBtn.addEventListener("click", handleClick);
-    
+// function handleClick() {
+//     window.location.reload();
+// }
+
+// refreshBtn.addEventListener("click", handleClick);
+
 //on click: flip coin
-    document.querySelector('#flipButton').addEventListener('click', flip)
 
-    function flip (event){
-        let heads = document.querySelector('#heads')
-        let tails = document.querySelector('#tails')
-        let result = document.getElementsByClassName("coin");
-        var x = Math.floor(Math.random() * 2);
-        console.log(x);
-        if (x == 0) {
+
+document.querySelector('#flipButton').addEventListener('click', flip)
+
+function flip(event) {
+    let heads = document.querySelector('#heads')
+    let tails = document.querySelector('#tails')
+    let result = document.getElementsByClassName("coin");
+    var x = Math.floor(Math.random() * 2);
+    console.log(x);
+    if (x == 0) {
         document.querySelector('#heads').classList.toggle = 'hidden'
-        result.innerHTML= "<img alt=\"the frontside of a coin\" src=\"img/heads.webp\" />";
+        result.innerHTML = "<img alt=\"the frontside of a coin\" src=\"img/heads.webp\" />";
         tails.style.display = 'none'
-    } else if (x == 1){
+    } else if (x == 1) {
         document.querySelector('#tails').classList.toggle = 'hidden'
-        result.innerHTML= "<img alt=\"the backside of a coin\" src=\"img/tails.webp\" />";
+        result.innerHTML = "<img alt=\"the backside of a coin\" src=\"img/tails.webp\" />";
         heads.style.display = 'none'
-      }
+    }
 }
 
 // document.querySelector('#flipButton').addEventListener('click', flip)
